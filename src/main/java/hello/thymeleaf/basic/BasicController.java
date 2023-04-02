97f95f7ef866f9da37d368543dc01ce06e337d67
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-03 05:39:23 +0900

    텍스트 - text, utext 와 변수 - SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -18,5 +24,36 @@ public class BasicController {
     public String textUnescped(Model model) {
         model.addAttribute("data","Sim<b>Sim</b>Hae");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("variable")
+    public String variable(Model model) {
+        User user1 = new User("user1", 20);
+        User user2 = new User("user2", 26);
+
+         List<User> list= new ArrayList<>();
+         list.add(user1);
+         list.add(user2);
+
+         Map<String,User> map  = new HashMap<>();
+         map.put("user1",user1);
+         map.put("user2",user2);
+
+         model.addAttribute("user", user1);
+         model.addAttribute("users", list);
+         model.addAttribute("userMap", map);
+
+         return "basic/variable";
+    }
+
+    @Data
+    static class User{
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
