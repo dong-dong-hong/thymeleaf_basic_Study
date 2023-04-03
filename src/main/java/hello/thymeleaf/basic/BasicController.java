658e2eb1c1365ec2dfd05a9c5b49d79c7f497ab6
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-03 10:00:00 +0900

    컨트롤러

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,16 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -44,6 +49,41 @@ public class BasicController {
          model.addAttribute("userMap", map);
 
          return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObject(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+        // 타임리프 유틸리티 객체들
+        //#message : 메시지, 국제화 처리
+        //#uris : URI 이스케이프 지원
+        //#dates : java.util.Date 서식 지원
+        //#calendars : java.util.Calendar 서식 지원
+        //#temporals : 자바8 날짜 서식 지원
+        //#numbers : 숫자 서식 지원
+        //#strings : 문자 관련 편의 기능
+        //#objects : 객체 관련 기능 제공
+        //#bools : boolean 관련 기능 제공
+        //#arrays : 배열 관련 기능 제공
+        //#lists , #sets , #maps : 컬렉션 관련 기능 제공
+        //#ids : 아이디 처리 관련 기능 제공
     }
 
     @Data
